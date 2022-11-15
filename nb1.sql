2e38342795b0160242e7ee62d926fc163131c277
Farsang Balázs <farsang.balazs617@gmail.com>
2022-11-15 20:23:25 +0100

    Basic authentication.

--- nb1.sql
+++ nb1.sql
@@ -374,3 +374,68 @@ INSERT INTO labdarugo(id,mezszam,klubid,posztid,utonev,vezeteknev,szulido,magyar
 INSERT INTO labdarugo(id,mezszam,klubid,posztid,utonev,vezeteknev,szulido,magyar,ertek) VALUES (316,23,5,12,'Bence','Banó-Szabó','1999.07.25',-1,100);
 INSERT INTO labdarugo(id,mezszam,klubid,posztid,utonev,vezeteknev,szulido,magyar,ertek) VALUES (317,89,5,8,'Balázs','Villám','1989.06.02',-1,175);
 INSERT INTO labdarugo(id,mezszam,klubid,posztid,utonev,vezeteknev,szulido,magyar,ertek) VALUES (318,87,1,9,'Lubos','Kamenar','1987.06.17',0,250);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+                         `id` int(11) NOT NULL auto_increment,
+                         `name` varchar(100) NOT NULL,
+                         `email` varchar(100) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+                                                            (1, 'Admin', 'admin@gmail.com', '$2a$10$QEaf3I.eLiZC4F4pDnqmC.sTysFlJ59wgROmw3ATxceFs/wgg0LvK'),
+                                                            (3, 'User', 'user@gmail.com', '$2a$10$exVjZOnYQ3oFdNTFP7qVHOoL8K2XhKpWXY3r8duw8v9pTNxmC0qbm');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `roles`
+--
+
+CREATE TABLE `roles` (
+                         `id` int(11) NOT NULL auto_increment,
+                         `name` varchar(255) NOT NULL,
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`) VALUES
+                                       (1, 'ROLE_ADMIN'),
+                                       (2, 'ROLE_ACTUATOR'),
+                                       (3, 'ROLE_USER');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user_role`
+--
+
+CREATE TABLE `user_role` (
+                             `user_id` int(11) NOT NULL,
+                             `role_id` int(11) NOT NULL,
+                             PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `user_role`
+--
+
+INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
+                                                   (1, 1),
+                                                   (1, 2),
+                                                   (1, 3),
+                                                   (3, 2);
